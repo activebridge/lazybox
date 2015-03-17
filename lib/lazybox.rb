e4e7610c0463dff312a4a6e705a0ea822a30acda
galulex <galulex@gmail.com>
2015-03-17 12:31:01 +0200

    Add class options

--- lib/lazybox.rb
+++ lib/lazybox.rb
@@ -7,7 +7,7 @@ module Lazybox
 
     def render_lazybox(options = {})
       content_tag(:div, '', id: :lazy_overlay, class: ('active' if content_for?(:lazybox))) do
-        content_tag(:div, id: :lazybox) do
+        content_tag(:div, id: :lazybox, class: options[:class]) do
           button_tag('', id: :lazy_close, class: :visible) +
           content_tag(:div, id: :lazy_body) do
             content_for(:lazybox)
